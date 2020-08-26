1a153ea5e19b23c0af93f763e75ea1ea563b4b11
Eliel Barrios <elielbarrios97@gmail.com>
2020-08-26 00:23:47 -0600

    Se corrigio el script de la bd

--- BasedeDatos/ScriptTablasBD.sql
+++ BasedeDatos/ScriptTablasBD.sql
@@ -15,31 +15,32 @@ municipio VARCHAR(50),
 direccion VARCHAR(50),
 detalles  VARCHAR(50),
 fk_id_usuario INT,
-FOREIGN KEY (fk_id_usuario) REFERENCES usuario (fk_id_usuario)
+FOREIGN KEY (fk_id_usuario) REFERENCES usuario (id_usuario)
 );
 
 CREATE TABLE producto (
 id_producto INT PRIMARY KEY AUTO_INCREMENT,
 descripcion VARCHAR(50),
 nombre VARCHAR(50),
-precio DOUBLE,
+precio DOUBLE
 );
 
 
 CREATE TABLE orden_producto (
-id_orden INT PRIMARY KEY AUTO_INCREMENT,
+id_orden_producto INT PRIMARY KEY AUTO_INCREMENT,
 fk_id_producto INT,
 cantidad INT,
-FOREIGN KEY (fk_id_producto) REFERENCES producto(id_producto)
+FOREIGN KEY (fk_id_producto) REFERENCES producto (id_producto)
 );
 
 CREATE TABLE orden (
+id_orden INT PRIMARY KEY AUTO_INCREMENT,
 fecha_orden DATE,
 fk_id_usuario INT,
-fk_id_orden INT,
+fk_id_ordenproducto INT,
 estado_orden VARCHAR(50),
 FOREIGN KEY (fk_id_usuario) REFERENCES usuario(id_usuario),
-FOREIGN KEY (fk_id_orden) REFERENCES carrito(id_orden)
+FOREIGN KEY (fk_id_ordenproducto) REFERENCES orden_producto(id_orden_producto)
 );
 
 CREATE TABLE categoria (
@@ -47,4 +48,11 @@ id_categoria INT PRIMARY KEY AUTO_INCREMENT,
 nombre_categoria VARCHAR(50),
 root_id INT,
 FOREIGN KEY (root_id) REFERENCES categoria(id_categoria)
+);
+
+CREATE TABLE detalle_producto_categoria (
+fk_id_producto INT,
+fk_id_categoria INT,
+foreign key (fk_id_producto) REFERENCES producto (id_producto),
+foreign key (fk_id_categoria) references categoria (id_categoria)
 );
