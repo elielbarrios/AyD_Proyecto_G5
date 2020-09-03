3f6dccb49f640bbc82e6d217192b02334244b2e1
Alvmilio <emilio0497@outlook.com>
2020-09-03 11:40:17 -0600

    Se corrigieron errores en productController y categoryController

--- BackEnd/controllers/categoryController.ts
+++ BackEnd/controllers/categoryController.ts
@@ -11,20 +11,20 @@ class CategoryController
         res.json(retVal);
     }
 
-    async createCategory(res : Response, req : Request) : Promise<void> //creacion de una nueva categoria
+    async createCategory(req : Request, res : Response) : Promise<void> //creacion de una nueva categoria
     {
         const retVal =  await db.query('INSERT INTO category set ?', [req.body]); //valor a retornar
         res.json(retVal);
     }
 
-    async updateCategory(res : Response, req : Request) : Promise<void>///actualizar una categoria en especifico
+    async updateCategory(req : Request, res : Response) : Promise<void>///actualizar una categoria en especifico
     {
         
         const retVal = await db.query('UPDATE categoria SET nombre_categoria = ? WHERE id_categoria = ?', [req.body.nombre_categoria, req.body.id_categoria] ); //valor a retornar
         res.json(retVal);
     }
 
-    async deleteCategory(res : Response, req : Request) : Promise<void>//eliminar una categoria en especifico
+    async deleteCategory(req : Request, res : Response) : Promise<void>//eliminar una categoria en especifico
     {
         const retVal = await db.query('DELETE FROM categoria WHERE id_categoria = ?', [req.params.id]); //valor a retornar
         res.json(retVal);
--- BackEnd/controllers/productController.ts
+++ BackEnd/controllers/productController.ts
@@ -4,37 +4,37 @@ import db from '../conf/database';
 class ProductController
 {
 
-    async getProducts(res : Response, req : Request) : Promise<void> //listar todos los productos
+    async getProducts(req : Request, res : Response ) : Promise<void> //listar todos los productos
     {
-        const retVal = await db.query('SELECT * FROM Producto', [req.body]); ///valor a retornar
+        const retVal = await db.query('SELECT * FROM producto', [req.body]); ///valor a retornar
         res.json(retVal);
     }
 
-    async getProduct(res : Response, req : Request) : Promise<void> //obtener la informacion de un producto en especifico
+    async getProduct(req : Request, res : Response) : Promise<void> //obtener la informacion de un producto en especifico
     {
-        const retVal = await db.query('SELECT * FROM Producto WHERE id_producto = ?', [req.params.id]);//valor a retornar
+        const retVal = await db.query('SELECT * FROM producto WHERE id_producto = ?', [req.params.id]);//valor a retornar
         res.json(retVal);
     }
     
-    async addProduct(res : Response, req : Request) : Promise<void> ///agregar un producto nuevo 
+    async addProduct(req : Request, res : Response) : Promise<void> ///agregar un producto nuevo 
     {
-        const retVal = await db.query('INSERT INTO Producto set ?', [req.body])
+        const retVal = await db.query('INSERT INTO producto set ?', [req.body])
         res.json(retVal);
     }
 
-    async updateProduct(res : Response, req : Request) : Promise<void>///actualizar los datos de un producto en especifico
+    async updateProduct(req : Request, res : Response) : Promise<void>///actualizar los datos de un producto en especifico
     {
         const retVal = await db.query('UPDATE producto SET ? WHERE id_producto = ?', [req.body, req.params.id] ); //valor a retornar
         res.status(200).json(retVal);
     }
 
-    async deleteProduct(res : Response, req : Request) : Promise<void>//eliminar un producto  en especifico
+    async deleteProduct(req : Request, res : Response) : Promise<void>//eliminar un producto  en especifico
     {
         const retVal = await db.query('DELETE FROM producto WHERE id_producto = ?', [req.params.id]); //valor a retornar
         res.status(200).json(retVal);
     }
 
-    async deleteAllProducts(res : Response, req : Request) : Promise<void>//eliminar todos los productos
+    async deleteAllProducts(req : Request, res : Response) : Promise<void>//eliminar todos los productos
     {
         const retVal = await db.query('DELETE FROM producto'); //valor a retornar
         res.status(200).json(retVal);
