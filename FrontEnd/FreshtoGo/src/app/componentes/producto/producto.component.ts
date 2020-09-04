8910bacb9f2eab0bd83d4edbfc7781eac397ceb0
coradowilly <coradowilly@gmail.com>
2020-09-04 01:32:25 -0600

    consumiendo con el backend

--- FrontEnd/FreshtoGo/src/app/componentes/producto/producto.component.ts
+++ FrontEnd/FreshtoGo/src/app/componentes/producto/producto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
+import { ServicioProductoService } from '../../servicios/servicio-producto.service'
+import { Producto } from '../../modelos/producto'
 
 @Component({
   selector: 'app-producto',
@@ -9,19 +11,87 @@ export class ProductoComponent implements OnInit {
 
   @HostBinding('class') classes = 'row';
   
-  constructor() { }
-
+  listadoProductos:any = [];
   selected:string = "";
-
-  ngOnInit(): void {
+  idDelete:any;
+  newProduct:Producto = {
+    id_producto:0,
+    descripcion:"",
+    nombre:"",
+    imagen:"",
+    precio:0
   }
 
-  seleccionar(){
-    this.selected = "texto";
+  selectProduct:Producto = {
+    id_producto:0,
+    descripcion:"",
+    nombre:"",
+    imagen:"",
+    precio:0
+  }
+
+  constructor(private productoServicio:ServicioProductoService) { }
+
+  ngOnInit(): void 
+  {
+    this.getCategorys();
+    console.log(this.listadoProductos);
+  }
+
+  getCategorys():any
+  {
+    this.productoServicio.getProducts()
+    .subscribe(
+      res => {
+        this.listadoProductos = res;
+      },
+      err => console.error(err)
+    );
+  }
+
+  seleccionar(item){
+    console.log(item)
+    this.selectProduct.nombre = item.nombre;
+    this.selectProduct.descripcion = item.descripcion;
+    this.selectProduct.imagen = item.imagen;
+    this.selectProduct.precio = item.precio;
+  }
+
+  selectDelete(id){
+    console.log(id)
+    this.idDelete = id.id_producto;
+    this.selected = id.nombre;
   }
 
   delete(){
-    
+    console.log(this.idDelete);
+    this.productoServicio.deleteProduct(this.idDelete);
+  }
+
+  create(){
+    let product:Producto={
+      nombre: this.newProduct.nombre,
+      descripcion: this.newProduct.descripcion,
+      imagen: this.newProduct.imagen,
+      precio: this.newProduct.precio
+    }
+    if(product.nombre != "" && product.descripcion != "" && product.imagen != "" && product.precio != 0)
+    {
+      this.productoServicio.createProduct(this.newProduct);
+    }
+  }
+
+  update(){
+    let product:Producto={
+      nombre: this.selectProduct.nombre,
+      descripcion: this.selectProduct.descripcion,
+      imagen: this.selectProduct.imagen,
+      precio: this.selectProduct.precio
+    }
+    if(product.nombre != "" && product.descripcion != "" && product.imagen != "" && product.precio != 0)
+    {
+      this.productoServicio.updateProduct(this.selectProduct);
+    }
   }
 
 }
---
+++ FrontEnd/FreshtoGo/src/app/servicios/servicio-producto.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Producto } from '../modelos/producto'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicioProductoService {
+
+  API_URI = 'http://52.15.119.221:3000/api/products';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(){
+    return this.http.get(`${this.API_URI}`);
+  }
+
+  createProduct(category:Producto){
+    return this.http.post(`${this.API_URI}`,category);
+  }
+
+  updateProduct(category:Producto){
+    return this.http.put(`${this.API_URI}`,category);
+  }
+
+  deleteProduct(id:any){
+    return this.http.delete(`${this.API_URI}/${id}`);
+  }
+}
