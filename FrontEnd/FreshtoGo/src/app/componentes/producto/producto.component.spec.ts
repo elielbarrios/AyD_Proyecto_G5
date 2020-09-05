34f69cf959713d0df165c6107e7c0f44f040d7f5
NeDGUndPanzer <tesco.herrera.edu@gmail.com>
2020-09-05 02:23:52 +0200

    se agregaron pruebas unitarias al CRUD de productos

--- FrontEnd/FreshtoGo/src/app/componentes/producto/producto.component.spec.ts
+++ FrontEnd/FreshtoGo/src/app/componentes/producto/producto.component.spec.ts
@@ -10,10 +10,18 @@ describe('ProductoComponent', () => {
   let component: ProductoComponent;
   let fixture: ComponentFixture<ProductoComponent>;
 
+  let prueba_tipoProducto:Producto = {
+    id_producto: 1024,
+    descripcion: "prueba",
+    nombre: "prueba",
+    imagen: "prueba",
+    precio: 1024
+  } 
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [Producto,RouterTestingModule,HttpClientTestingModule],
-      providers: [ServicioProductoService],
+      imports: [RouterTestingModule,HttpClientTestingModule],
+      providers: [ServicioProductoService, Producto],
       declarations: [ProductoComponent]
     })
     .compileComponents();
@@ -31,10 +39,61 @@ describe('ProductoComponent', () => {
 
   // Pruebas Unitarias
 
-  describe('Test for Producto -> getProduct()',() => {
-    it('Should return undefined',() => {
-      expect(component.getProducts()).toEqual(undefined);
-    });
+  describe('Test for Producto -> getProduct()',() => 
+  {
+      it('Should change the listadoProductos var',() => 
+      {
+          let componente_listadoProductos = component.listadoProductos;
+          component.getProducts();
+          expect(component.listadoProductos).toEqual(componente_listadoProductos);
+      });
+  });
+
+  describe('Test for Producto -> seleccionar()',() => 
+  {
+      it('Should change the component.selectProduct variable',() => {
+        let prueba:Producto = {
+          id_producto:0,
+          descripcion:"",
+          nombre:"",
+          imagen:"",
+          precio:0
+        }; 
+        component.seleccionar(prueba_tipoProducto);
+        expect(prueba).not.toEqual(component.selectProduct);
+      });
+  });
+
+  describe('Test for Producto -> selectDelete()',() => 
+  {
+      it('Should change the component.idDelete and component.selected variables',() => {
+          let Component_var_idDelete:any = component.idDelete;
+          let Component_var_selected:string = component.selected;    
+          component.selectDelete(prueba_tipoProducto);
+          expect(Component_var_idDelete).not.toEqual(component.idDelete);
+      });
+  });
+
+  describe('Test for Producto -> delete()',() => 
+  {
+      it('Should use the service to send a delete instruction',() => {
+        component.delete();
+        expect(component.selected).toEqual("");
+      });
+  });
+
+  describe('Test for Producto -> create()',() => 
+  {
+      it('Should use the var component.newProduct where the var  is not equal to null',() => {
+        expect(component.newProduct.nombre).not.toEqual("null");
+      });
+  });
+
+  describe('Test for Producto -> update()',() => 
+  {
+      it('Should use the var component.selectProduct where the var  is not equal to null',() => {
+        expect(component.selectProduct.nombre).not.toEqual("null");
+      });
   });
 
 });
