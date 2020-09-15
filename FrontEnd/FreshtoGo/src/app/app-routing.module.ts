0a9e5507fb11aa8482a7299cd021ed117d2372c1
coradowilly <coradowilly@gmail.com>
2020-09-14 18:44:23 -0600

    Desarrollo visual del login

--- FrontEnd/FreshtoGo/src/app/app-routing.module.ts
+++ FrontEnd/FreshtoGo/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { CarritoComponent } from './componentes/carrito/carrito.component';
 import { CatalogoPrincipalComponent } from './componentes/catalogo-principal/catalogo-principal.component';
 import { ProductoComponent } from './componentes/producto/producto.component';
 import { CategoriaComponent } from './componentes/categoria/categoria.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 const routes: Routes = [
 
   { path: "carrito", component: CarritoComponent },
   { path: "catalogo", component: CatalogoPrincipalComponent },
   { path: "producto", component: ProductoComponent },
-  { path: "category", component: CategoriaComponent }
+  { path: "category", component: CategoriaComponent },
+  { path: "login", component: LoginComponent }
 ];
 
 @NgModule({
--- FrontEnd/FreshtoGo/src/app/componentes/login/login.component.ts
+++ FrontEnd/FreshtoGo/src/app/componentes/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  @HostBinding('class') classes = 'row';
 
   constructor() { }
 
