e73ad1fce1b467538dfc06b38d3e3179c336246f
Alvmilio <emilio0497@outlook.com>
2020-09-25 22:59:34 -0600

    TDD - Implementacion de funcionalidad

--- BackEnd/controllers/facturacionController.ts
+++ BackEnd/controllers/facturacionController.ts
@@ -6,12 +6,15 @@ class FacturacionController
 
     async getMetodosDePago(req : Request, res : Response) : Promise<void> //obtener todas las Categorias
     {
-        
+        var userid = req.body.userid;
+        const retVal = await db.query('SELECT * FROM Tarjeta WHERE fk_id_usuario = ?', [userid]); ///valor a retornar
+        res.json(retVal);
     }
     
     async addMetodoDePago(req : Request, res : Response) : Promise<void> //obtener todas las Categorias
     {
-        
+        const retVal = await db.query('INSERT INTO Tarjeta set ?', [req.body])
+        res.json(retVal);
     }
 
     
