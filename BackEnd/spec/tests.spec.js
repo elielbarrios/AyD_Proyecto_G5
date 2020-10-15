0db9756b980575dd1772fcf3ea002c866adaa06b
Saul Guzman <guzmanmsaul@gmail.com>
2020-10-15 00:22:45 -0600

    BDD,Prueba Unitaria para Personalizacion

--- BackEnd/spec/tests.spec.js
+++ BackEnd/spec/tests.spec.js
@@ -258,6 +258,37 @@ describe("Pruebas unitarias", () => {
     });
 
 
+
+     // Personalizacion
+     describe("GIVEN: El usuario quiere editar su cuenta", () => { 
+
+        describe("WHEN: envia sus nuevos valores", ()=>{
+            var enviar = {id_usuario:'1',nombre: 'Alvin', apelldo: 'Espino',email : 'alvine@gmail.com',password:'54321',celular:'88877700',nit:'1235-5',direccion:'usac ingenieria'};
+            var estado;
+            var verifi;
+            it("THEN: Retorna un mensaje de verificacion", (done)=>{
+   
+                Request.post("http://localhost:3000/api/editarperdil", {form:enviar} , (error, response, body) => {
+                    estado = JSON.parse(response.body).estado;
+                    verifi= JSON.parse(response.body).mensaje;
+                   if(estado === 1){
+                    expect(verifi).toBe("Editado Exitoso");
+                    done();
+                   }else{
+                    expect(verifi).toBe("Fallo Edicion");
+                    done();
+                   }
+
+                })
+
+
+            });
+
+        });
+    
+    });
+
+
     
 });
 
